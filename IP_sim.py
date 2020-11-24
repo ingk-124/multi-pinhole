ce1c616aed0ca2b8f7ad4206999e13cb8dec9f91
inagakishinichiro <sinina124aikido@gmail.com>
2020-11-24 23:13:33 +0900

    Bug fix

--- IP_sim.py
+++ IP_sim.py
@@ -245,8 +245,9 @@ class OpticalSystem:
         #     _ = list(tqdm(pmap, total=len(self.plasma_data.parameters)))
         mode_arr = np.array(self.plasma_data.parameters, dtype='O')
         # breakpoint()
-        # Parallel(n_jobs=-1, prefer='threads')([delayed(self.fb_image)(p) for p in tqdm(self.plasma_data.parameters)])
-        Parallel(n_jobs=-1, verbose=10)([delayed(self.fb_image)(p) for p in self.plasma_data.parameters])
+        Parallel(n_jobs=-1, verbose=10, prefer='threads')(
+            [delayed(self.fb_image)(p) for p in self.plasma_data.parameters])
+        # Parallel(n_jobs=-1, verbose=10)([delayed(self.fb_image)(p) for p in self.plasma_data.parameters])
         np.save(self.path / "mode_array.npy", mode_arr)
         n = int(np.log10(len(self.plasma_data.parameters)) + 1)
         with open(self.path / "mode_list.txt", "w") as f:
