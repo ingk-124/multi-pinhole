5b79e666c40f25a91c2e39b4a536254bc947ec0b
inagakishinichiro <sinina124aikido@gmail.com>
2020-12-14 10:50:58 +0900

    fix: calc.py/Calculation/cross_sections
    Change default ylim2

--- calc.py
+++ calc.py
@@ -56,7 +56,7 @@ class Calculation:
     def fb_img(self, n):
         return self.P * self.fb_mode(n)
 
-    def cross_sections(self, n_l, x=166, z=166, xlim1=(0, 758), ylim1=(-250, 250), xlim2=(0, 758), ylim2=(-250, 250),
+    def cross_sections(self, n_l, x=166, z=166, xlim1=(0, 758), ylim1=(-250, 250), xlim2=(0, 758), ylim2=(250, -250),
                        figsize=(5, 10), suptitle_size=40, title_size=25, suptitle="Cross sections",
                        pol_title="Poloidal", tor_title="Toroidal", image_title="Image"):
         c = "coolwarm"
@@ -126,7 +126,7 @@ class Calculation:
 
         return fig
 
-    def cross_section_d(self, d_l=None, n=0, x=166, z=166, xlim1=(0, 758), ylim1=(-250, 250), xlim2=(0, 758),
+    def cross_section_d(self, d_l=None, n=0, x=166, z=166, xlim1=(0, 758), ylim1=(250, -250), xlim2=(0, 758),
                         ylim2=(-250, 250), figsize=(5, 10), suptitle_size=40, title_size=25, suptitle="Cross sections",
                         pol_title="Poloidal", tor_title="Toroidal", image_title="Image"):
         if d_l is None:
